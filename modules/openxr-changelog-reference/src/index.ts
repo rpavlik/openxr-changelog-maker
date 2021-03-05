d1835e62b25c9a79a85a6311d1c9330ffb6e9c46
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-03-05 11:02:37 -0600

    module work

---
+++ modules/openxr-changelog-reference/src/index.ts
@@ -0,0 +1,17 @@
+// import {
+//     RefTypes,
+//     RefType,
+//     GitHubRepo,
+//     GitLabRepo,
+//     Repo,
+//     Repos,
+//     AllRepos,
+//     RepoHumanName,
+//     ValidRefTypes,
+//     GitLabReference,
+//     GitHubReference,
+//     StrictGeneralReference,
+//     GeneralReference,
+//     isReferenceValid
+// } from './reference'
+export * from './reference'
